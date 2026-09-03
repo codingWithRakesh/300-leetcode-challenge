65e6df5e23352e271ca1bfc3488c716a6221af8b
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2026-09-03 20:04:06 +0530

    add solution for Isomorphic Strings

---
+++ Hash Tables/Easy/Isomorphic Strings (205)/Solution3.java
@@ -0,0 +1,22 @@
+
+public class Solution3 {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
+        for(int i = 0; i < s.length(); i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(map1[c1] != map2[c2]) return false;
+            map1[c1] = i + 1;
+            map2[c2] = i + 1;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Solution3 s = new Solution3();
+        String str1 = "egg";
+        String str2 = "add";
+        System.out.println(s.isIsomorphic(str1,str2));
+    }
+}
